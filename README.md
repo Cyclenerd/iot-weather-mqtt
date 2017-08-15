d751a758da1e1087d0fd2eca1badc991c493d632
Nils <git@velohero.com>
2017-08-15 15:26:55 +0200

    init

---
+++ README.md
@@ -0,0 +1,133 @@
+# WiFi Weather Sensor that talks MQTT (SAP IoT Cloud Ready)
+
+With `iot-weather-mqtt.ino` an **ESP8266** and a **DHT22** you can build a weather station with Internet.
+So you build an Internet of Things (IoT) 🤓.
+The sensor data are published by MQTT protocol.
+The format corresponds to the expected format of the SAP IoT Cloud ☁️.
+You can save the sensor data quickly and easily in the SAP Cloud and process it there.
+
+![Hardware](https://www.nkn-it.de/iot-weather-mqtt/hardware.jpg?v1)
+
+
+## How does it work?
+
+* The ESP8266 connects itself via WiFi and gets by DHCP an IP address
+* The computer connects to the MQTT broker `iot.eclipse.org`
+	* MQTT is explained in [Wikipedia](https://en.wikipedia.org/wiki/MQTT)
+* Sensor data is sent as JSON (SAP IoT Cloud format) to the defined MQTT topic
+
+
+## Requirements
+
+You need:
+
+* One Arduino with ESP8266
+    * I use the `NodeMCU Lua Lolin V3 Module`
+* One DHT22 sensor
+* Three LEDs
+* A few cables and a breakout board to connect everything
+
+Costs about 15 EUR in Europe.
+
+
+## Set-Up
+
+The programming is done with the Arduino IDE. If you do not have it yet, install it.
+
+Add in Preferences the "Additional Boards Manager URL":
+`http://arduino.esp8266.com/stable/package_esp8266com_index.json`
+
+![Preferences](https://www.nkn-it.de/iot-weather-mqtt/esp8266_url.jpg?v1)
+
+
+### NodeMCU
+
+Select the right board:
+
+![NodeMCU](https://www.nkn-it.de/iot-weather-mqtt/nodemcu.jpg?v1)
+
+
+### Libraries
+
+Install the required libraries. Search for:
+
+* DHT
+* ESP8266WiFi
+* PubSubClient
+* ArduinoJson
+* Adafruit Unified Sensor Driver
+
+![Libraries](https://www.nkn-it.de/iot-weather-mqtt/libraries.jpg?v1)
+
+
+### 🚨 Increase the MQTT library's memory limit 🚨
+
+Open the `PubSubClient.h` file from the Arduino libraries folder.
+Increasing `MQTT_MAX_PACKET_SIZE` to `256`.
+
+On a Mac, this will be located at:
+`~/Documents/Arduino/libraries/PubSubClient/src/PubSubClient.h`
+
+## Wiring
+
+Connect the DHT22 data pin to `D6`.
+
+```
+  _______
+ /  ( )  \
+ +-------+
+ | [] [] |
+ | [] [] |
+ | [] [] |
+ +-------+
+  | | | |
+  
+   \ \ \ \__ Ground: Connect it to GND
+    \ \ \___ Nothing
+     \ \____ Data pin: Connect it to D6
+      \_____ Positive pin: Connect it to 3v3
+```
+
+Connect `D0`, `D1`, `D2` each with one LED.
+
+
+## Program
+
+Open `iot-weather-mqtt.ino` with the Arduino IDE and adjust the configuration:
+
+* `ssid`           : Your WiFi SSID
+* `password`       : Your WiFi password
+* `sap_iot_msg_id` : If you use the SAP IoT Cloud, the message type must be entered here
+* `mqtt_topic`     : If you use the SAP IoT Cloud, the structure must be as follows: `iot/data/iotmms<ACCOUNT>/v1/<DEVICE>`
+
+You find the data in the SAP Internet of Things Service Cockpit:
+
+![sap_iot_msg_id](https://www.nkn-it.de/iot-weather-mqtt/sap_iot_msg_id.jpg?v1)
+
+![Device ID](https://www.nkn-it.de/iot-weather-mqtt/device_id.jpg?v1)
+
+![mqtt_topic](https://www.nkn-it.de/iot-weather-mqtt/mqtt_topic.jpg?v1)
+
+Upload `iot-weather-mqtt.ino` to your ESP8266.
+
+When everything is working, all LEDs light up:
+
+* `D0` : Power on
+* `D1` : Connecting to WiFi
+* `D2` : MQTT connected
+
+
+Messages should appear on the MQTT Topic. Here's an example:
+
+```
+{
+	"mode":"async",
+	"messageType":"bd03144608253c6a4fca",
+	"messages":[
+		{"celcius":23.4,"fahrenheit":74.12,"humidity":67.59999}
+	]
+}
+```
+## Help 👍
+
+If you have found a bug (English is not my mother tongue) or have any improvements, send me a pull request.
---
+++ iot-weather-mqtt.ino
@@ -0,0 +1,212 @@
+/*
+  With this program you can read temperature and humidity.
+  The data is sent to an MQTT broker.
+  The preparation of the data corresponds to the SAP IoT format.
+
+  You need an ESP8266.
+  Connect D0, D1, D2 each with one LED.
+  Connect D6 to the DHT22 sensor.
+
+  Open the PubSubClient.h file from the Arduino libraries folder.
+  On a Mac, this will be located at:
+  ~/Documents/Arduino/libraries/PubSubClient/src/PubSubClient.h
+  Increasing MQTT_MAX_PACKET_SIZE to 256 
+  
+  More details: https://github.com/Cyclenerd/iot-weather-mqtt
+ */
+
+#include <DHT.h>          // https://github.com/adafruit/DHT-sensor-library and
+                          // https://github.com/adafruit/Adafruit_Sensor
+#include <ESP8266WiFi.h>  
+#include <PubSubClient.h> // https://pubsubclient.knolleary.net/
+#include <ArduinoJson.h>  // https://bblanchon.github.io/ArduinoJson/
+
+/***************************************************************************************
+   Configuration Section
+ ***************************************************************************************/
+
+// WiFi SSID Name
+const char* ssid     = "YOUR-WIFI-SSID";
+// WiFi Password
+const char* password = "YOUR-WIFI-PASSWORD";
+
+// SAP IoT Message ID
+const char* sap_iot_msg_id = "<SAP-MESSAGE-ID>";
+
+// MQTT Topic
+const char* mqtt_topic = "iot/data/iotmms<ACCOUNT>/v1/<DEVICE>";
+
+// MQTT Server
+const char* mqtt_server      = "iot.eclipse.org";
+short unsigned int mqtt_port = 1883; // unencrypted
+
+/***************************************************************************************
+   End Configuration Section
+ ***************************************************************************************/
+
+#define DHTPIN D6     // what digital pin the DHT22 is conected to
+#define DHTTYPE DHT22 // there are multiple kinds of DHT sensors
+
+DHT dht(DHTPIN, DHTTYPE);
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+// The setup function runs once when you press reset or power the board
+void setup() {
+  Serial.begin(115200);
+  delay(1000); // wait 1 seconds
+
+  // Initialize digital pins an output.
+  pinMode(D0, OUTPUT);
+  pinMode(D1, OUTPUT);
+  pinMode(D2, OUTPUT);
+  pinMode(D3, OUTPUT);
+  pinMode(D4, OUTPUT);
+
+  test_led();
+
+  digitalWrite(D0, HIGH); // LED D0 on
+  connect();
+}
+
+// The connect function makes a connection with the WiFi and the MQTT server
+void connect() {
+  digitalWrite(D1, LOW); // LED D1 off
+  digitalWrite(D2, LOW); // LED D2 off
+  
+  // Start connecting to WiFi network
+  Serial.println();
+  Serial.print("Connecting to WiFi name ");
+  Serial.println(ssid);
+
+   // WiFi fix: https://github.com/esp8266/Arduino/issues/2186
+  WiFi.persistent(false);
+  WiFi.mode(WIFI_OFF);
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+     if (WiFi.status() == WL_CONNECT_FAILED) {
+      Serial.println("Failed to connect to WIFI. Please verify credentials!");
+      Serial.println();
+    }
+    delay(500);
+    Serial.print(".");
+  }
+
+  // Connected to WiFi
+  digitalWrite(D1, HIGH); // LED D1 on
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  // Setup MQTT client
+  client.setServer(mqtt_server, mqtt_port);
+
+  Serial.println();
+  Serial.print("Connecting to MQTT server ");
+  Serial.println(mqtt_server);
+
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    // Attempt to connect
+    if (client.connect("iot-weather-mqtt")) {
+      digitalWrite(D2, HIGH); // LED D2 on
+      Serial.println();
+      Serial.println("MQTT connected");
+      client.publish(mqtt_topic, "hello");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      delay(5000); // Wait 5 seconds before retrying
+    }
+  }
+}
+
+// The test_led function tests the connected LEDs on D0, D1 and D2
+void test_led() {
+  digitalWrite(D0, HIGH);
+  digitalWrite(D1, HIGH);
+  digitalWrite(D2, HIGH);
+  delay(5000); // wait 5 seconds
+  digitalWrite(D0, LOW);
+  digitalWrite(D1, LOW);
+  digitalWrite(D2, LOW);
+}
+
+// The report function sends the data to the MQTT topic
+void report(double humidity, double tempC, double tempF, double heatIndexC, double heatIndexF) {
+  StaticJsonBuffer<500> jsonBuffer;
+  JsonObject& rootObject = jsonBuffer.createObject();
+  rootObject["mode"] = "async";
+  rootObject["messageType"] = sap_iot_msg_id;
+  JsonArray& messagesArray = rootObject.createNestedArray("messages");
+  JsonObject& messageObject = messagesArray.createNestedObject();
+  messageObject["celcius"] = tempC;
+  messageObject["fahrenheit"] = tempF;
+  messageObject["humidity"] = humidity;
+  Serial.print("State: ");
+  //rootObject.printTo(Serial);
+  //Serial.println();
+
+  char mqtt_buffer[256];
+  rootObject.printTo(mqtt_buffer, sizeof(mqtt_buffer));
+  Serial.println(mqtt_buffer);
+  client.publish(mqtt_topic, mqtt_buffer);
+}
+
+int timeSinceLastRead = 0;
+
+// The loop function runs over and over again forever
+void loop() {
+  bool toReconnect = false;
+
+  if (WiFi.status() != WL_CONNECTED) {
+    Serial.println("Disconnected from WiFi");
+    toReconnect = true;
+  }
+
+  if (!client.connected()) {
+    Serial.println("Disconnected from MQTT");
+    Serial.println(client.state());
+    toReconnect = true;
+  }
+
+  if (toReconnect) {
+    connect();
+  }
+
+  client.loop();
+  
+  // Report every 5 seconds
+  if(timeSinceLastRead > 5000) {
+    // Reading temperature or humidity takes about 250 ms!
+    // Sensor readings may also be up to 2 seconds
+    float h = dht.readHumidity();
+    // Read temperature as Celsius (the default)
+    float c = dht.readTemperature();
+    // Read temperature as Fahrenheit (isFahrenheit = true)
+    float f = dht.readTemperature(true);
+
+    // Check if any reads failed and exit early (to try again).
+    if (isnan(h) || isnan(c) || isnan(f)) {
+      Serial.println("Failed to read from DHT sensor!");
+      timeSinceLastRead = 0;
+      return;
+    }
+
+    // Compute heat index in Fahrenheit (the default)
+    float hif = dht.computeHeatIndex(f, h);
+    // Compute heat index in Celsius (isFahreheit = false)
+    float hic = dht.computeHeatIndex(c, h, false);
+
+    report(h, c, f, hic, hif);
+
+    timeSinceLastRead = 0;
+  }
+  delay(500); // sleep 500 ms
+  timeSinceLastRead += 500;
+
+}
