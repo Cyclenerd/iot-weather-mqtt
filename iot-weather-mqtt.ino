a8cfe9ae1e3ad0e2b26d66303c25085a6afa83dd
Nils <git@velohero.com>
2017-08-21 17:25:02 +0200

    cleaned up a lot and improved reconnect

--- iot-weather-mqtt.ino
+++ iot-weather-mqtt.ino
@@ -13,11 +13,11 @@
   Increasing MQTT_MAX_PACKET_SIZE to 256 
   
   More details: https://github.com/Cyclenerd/iot-weather-mqtt
- */
+*/
 
+#include <ESP8266WiFi.h>  // https://github.com/esp8266/Arduino
 #include <DHT.h>          // https://github.com/adafruit/DHT-sensor-library and
                           // https://github.com/adafruit/Adafruit_Sensor
-#include <ESP8266WiFi.h>  // https://github.com/esp8266/Arduino
 #include <PubSubClient.h> // https://pubsubclient.knolleary.net/
 #include <ArduinoJson.h>  // https://bblanchon.github.io/ArduinoJson/
 
@@ -36,16 +36,21 @@ const char* sap_iot_msg_id = "<SAP-MESSAGE-ID>";
 // MQTT Topic
 const char* mqtt_topic = "iot/data/iotmms<ACCOUNT>/v1/<DEVICE>";
 
+
 // MQTT Server
-const char* mqtt_server      = "iot.eclipse.org";
-short unsigned int mqtt_port = 1883; // unencrypted
+const char* mqtt_server            = "iot.eclipse.org";
+const short unsigned int mqtt_port = 1883; // unencrypted
 
 // Report every n Minutes
-unsigned int report_every_min = 5; // 5 minutes
+const unsigned int report_every_min = 5; // 5 minutes
+
+// Publish Uptime to MQTT Topic
+const bool publish_uptime = true; // yes = true, no = false
 
 /***************************************************************************************
    End Configuration Section
  ***************************************************************************************/
+
 
 #define DHTPIN D6     // what digital pin the DHT22 is conected to
 #define DHTTYPE DHT22 // there are multiple kinds of DHT sensors
@@ -54,49 +59,79 @@ DHT dht(DHTPIN, DHTTYPE);
 WiFiClient espClient;
 PubSubClient client(espClient);
 
-// The setup function runs once when you press reset or power the board
-void setup() {
-  Serial.begin(115200);
-  delay(1000); // wait 1 seconds
+unsigned long time_msec;
+unsigned long last_report_msec;
+const unsigned int report_every_msec = report_every_min * 60 * 1000; // min to msec
 
-  // Initialize digital pins an output.
-  pinMode(D0, OUTPUT);
-  pinMode(D1, OUTPUT);
-  pinMode(D2, OUTPUT);
-  pinMode(D3, OUTPUT);
-  pinMode(D4, OUTPUT);
 
-  test_led();
+/***************************************************************************************
+ * Helpers 
+ ***************************************************************************************/
+ 
+// The test_led function tests the connected LEDs on D0, D1 and D2
+void test_led() {
+  digitalWrite(D0, HIGH);
+  digitalWrite(D1, HIGH);
+  digitalWrite(D2, HIGH);
+  delay(5000); // wait 5 seconds
+  digitalWrite(D0, LOW);
+  digitalWrite(D1, LOW);
+  digitalWrite(D2, LOW);
+}
 
-  digitalWrite(D0, HIGH); // LED D0 on
-  connect();
+// The uptime function keeps a uptime counter that will survive the 50 day timer rollover
+// https://www.arduino.cc/en/Reference/Millis
+unsigned long uptime_day       = 0;
+unsigned int  uptime_hour      = 0;
+unsigned int  uptime_min       = 0;
+unsigned int  uptime_sec       = 0;
+unsigned int  uptime_rollover  = 0;
+bool          uptime_high_msec = false;
+void uptime() {
+  // Making Note of an expected rollover
+  if (millis() >= 3000000000) {
+    uptime_high_msec = true;
+  }
+
+  // Making note of actual rollover
+  if (millis() <= 100000 && uptime_high_msec) {
+    uptime_rollover++;
+    uptime_high_msec = false;
+  }
+
+  unsigned long time_sec = millis() / 1000;
+  uptime_sec  = time_sec % 60;
+  uptime_min  = (time_sec / 60) % 60;
+  uptime_hour = (time_sec / (60 * 60)) % 24;
+  uptime_day  = (uptime_rollover * 50) + (time_sec / (60 * 60 * 24)); // First portion takes care of a rollover [around 50 days]
 }
 
 // The connect function makes a connection with the WiFi and the MQTT server
 void connect() {
   digitalWrite(D1, LOW); // LED D1 off
   digitalWrite(D2, LOW); // LED D2 off
-  
+
   // Start connecting to WiFi network
-  Serial.println();
-  Serial.print("Connecting to WiFi name ");
-  Serial.println(ssid);
+  if (WiFi.status() != WL_CONNECTED) {
+    Serial.println();
+    Serial.print("Connecting to WiFi name ");
+    Serial.println(ssid);
 
-   // WiFi fix: https://github.com/esp8266/Arduino/issues/2186
-  WiFi.persistent(false);
-  WiFi.mode(WIFI_OFF);
-  WiFi.mode(WIFI_STA);
-  WiFi.begin(ssid, password);
+    // WiFi fix: https://github.com/esp8266/Arduino/issues/2186
+    WiFi.persistent(false);
+    WiFi.mode(WIFI_OFF);
+    WiFi.mode(WIFI_STA);
+    WiFi.begin(ssid, password);
 
-  while (WiFi.status() != WL_CONNECTED) {
-    if (WiFi.status() == WL_CONNECT_FAILED) {
-      Serial.println("Failed to connect to WIFI. Please verify credentials!");
-      Serial.println();
+    while (WiFi.status() != WL_CONNECTED) {
+      if (WiFi.status() == WL_CONNECT_FAILED) {
+        Serial.println("Failed to connect to WIFI. Please verify credentials!");
+        Serial.println();
+      }
+      delay(500);
+      Serial.print(".");
     }
-    delay(500);
-    Serial.print(".");
   }
-
   // Connected to WiFi
   digitalWrite(D1, HIGH); // LED D1 on
   Serial.println("");
@@ -106,6 +141,7 @@ void connect() {
 
   // Setup MQTT client
   client.setServer(mqtt_server, mqtt_port);
+  //client.setCallback(callback);
 
   Serial.println();
   Serial.print("Connecting to MQTT server ");
@@ -128,27 +164,16 @@ void connect() {
   }
 }
 
-// The test_led function tests the connected LEDs on D0, D1 and D2
-void test_led() {
-  digitalWrite(D0, HIGH);
-  digitalWrite(D1, HIGH);
-  digitalWrite(D2, HIGH);
-  delay(5000); // wait 5 seconds
-  digitalWrite(D0, LOW);
-  digitalWrite(D1, LOW);
-  digitalWrite(D2, LOW);
-}
-
 // The report function sends the data to the MQTT topic
-void report(double humidity, double tempC, double tempF, double heatIndexC, double heatIndexF) {
+void report(double humidity, double temp_c, double temp_f, double heat_index_c, double heat_index_f) {
   StaticJsonBuffer<500> jsonBuffer;
   JsonObject& rootObject = jsonBuffer.createObject();
   rootObject["mode"] = "async";
   rootObject["messageType"] = sap_iot_msg_id;
   JsonArray& messagesArray = rootObject.createNestedArray("messages");
   JsonObject& messageObject = messagesArray.createNestedObject();
-  messageObject["celcius"] = tempC;
-  messageObject["fahrenheit"] = tempF;
+  messageObject["celcius"] = temp_c;
+  messageObject["fahrenheit"] = temp_f;
   messageObject["humidity"] = humidity;
   Serial.print("State: ");
   //rootObject.printTo(Serial);
@@ -157,70 +182,94 @@ void report(double humidity, double tempC, double tempF, double heatIndexC, doub
   char mqtt_buffer[256];
   rootObject.printTo(mqtt_buffer, sizeof(mqtt_buffer));
   Serial.println(mqtt_buffer);
+  // Publish
   client.publish(mqtt_topic, mqtt_buffer);
 }
 
-unsigned int report_every_msec = report_every_min * 60 * 1000; // min to msec
-unsigned int timeSinceLastRead = report_every_msec + 1;
-unsigned long time_msec;
+
+/***************************************************************************************
+ * Let's start 
+ ***************************************************************************************/
+
+// The setup function runs once when you press reset or power the board
+void setup() {
+  Serial.begin(115200);
+  delay(1000); // wait 1 seconds
+
+  // Initialize digital pins an output.
+  pinMode(D0, OUTPUT);
+  pinMode(D1, OUTPUT);
+  pinMode(D2, OUTPUT);
+  pinMode(D3, OUTPUT);
+  pinMode(D4, OUTPUT);
+
+  test_led();
+
+  digitalWrite(D0, HIGH); // LED D0 on
+  
+  connect();
+}
 
 // The loop function runs over and over again forever
 void loop() {
-  bool toReconnect = false;
-
+  uptime(); // get uptime since the board began running
+  bool reconnect = false;
   if (WiFi.status() != WL_CONNECTED) {
     Serial.println("Disconnected from WiFi");
-    toReconnect = true;
+    reconnect = true;
   }
-
   if (!client.connected()) {
     Serial.print("Disconnected from MQTT, rc=");
     Serial.println(client.state());
-    toReconnect = true;
+    reconnect = true;
   }
-
-  if (toReconnect) {
+  if (reconnect) {
     connect();
   }
 
-  client.loop();
-  
-  // Report every n minutes (report_every_min -> report_every_msec)
-  if(timeSinceLastRead > report_every_msec) {
-    // Prints time since program started
-    Serial.print("Uptime: ");
-    time_msec = millis();
-    Serial.print(time_msec);
-    Serial.println(" msec");
-    
+  // Status report ever n minutes
+  // millis() will overflow (go back to zero), after approximately 50 days
+  if (last_report_msec == 0 || millis() - last_report_msec >= report_every_msec || millis() < last_report_msec) {
+    // Uptime N days, HH:MM:SS
+    String uptime_string = "Uptime: " +
+                           String(uptime_day) + " days, " +
+                           (uptime_hour < 10 ? "0" : "") + String(uptime_hour) + ":" +
+                           (uptime_min < 10 ? "0" : "") + String(uptime_min) + ":" +
+                           (uptime_sec < 10 ? "0" : "") + String(uptime_sec);
+    Serial.print(uptime_string);
+    Serial.println();
+    // Publish uptime
+    if (publish_uptime) {
+      client.publish(mqtt_topic, uptime_string.c_str());
+    }
+
     // Reading temperature or humidity takes about 250 ms!
     // Sensor readings may also be up to 2 seconds
-    float h = dht.readHumidity();
+    float humidity = dht.readHumidity();
     // Read temperature as Celsius (the default)
-    float c = dht.readTemperature();
+    float temp_c = dht.readTemperature();
     // Read temperature as Fahrenheit (isFahrenheit = true)
-    float f = dht.readTemperature(true);
+    float temp_f = dht.readTemperature(true);
 
     // Check if any reads failed and exit early (to try again).
-    if (isnan(h) || isnan(c) || isnan(f)) {
+    if (isnan(humidity) || isnan(temp_c) || isnan(temp_f)) {
       Serial.println("Failed to read from DHT sensor!");
       delay(2000); // sleep 2 seconds
       return;
     }
 
     // Compute heat index in Fahrenheit (the default)
-    float hif = dht.computeHeatIndex(f, h);
+    float heat_index_f = dht.computeHeatIndex(temp_f, humidity);
     // Compute heat index in Celsius (isFahreheit = false)
-    float hic = dht.computeHeatIndex(c, h, false);
+    float heat_index_c = dht.computeHeatIndex(temp_c, humidity, false);
 
-    report(h, c, f, hic, hif);
+    report(humidity, temp_c, temp_f, heat_index_c, heat_index_f);
 
     Serial.print("Wait ");
     Serial.print(report_every_min);
     Serial.println(" minutes...");
-    timeSinceLastRead = 0;
+    last_report_msec = millis();
   }
-  
-  delay(500); // sleep 500 milliseconds
-  timeSinceLastRead += 500;
+
+  client.loop();
 }
